91fc2af8bb64096fc4dfb874835c1a2146494ac9
Vidsmo <vidsmo1506@gmail.com>
2026-04-29 19:40:45 +0530

    Update Dijkstra implementation

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -1,89 +1,86 @@
 package graph;
+
 import java.util.*;
 
 public class Dijkstra
-{
-static class Pair implements Comparable<Pair>
-// this is a java class that helps with comparison
-// we use this because in the priority queue, the pairs will come in and we need to arrange them in ascending order
-{
-int node;
-int path;
-public Pair(int node, int path)
-{
-this.node = node;
-this.path = path;
-}
-@Override
-// this is for comparing in ascending order, for descending order we can reverse it
-public int compareTo(Pair p2)
-{
-return Integer.compare(this.path, p2.path);
-}
-}
+ {
+    static class Pair implements Comparable<Pair>
+    // this is a java class that helps with comparison
+    // we use this because in the priority queue, the pairs will come in and we need
+    // to arrange them in ascending order
+    {
+        int node;
+        int path;
 
-public static List shortestPath(ArrayList<edge>[] graph, node[] nodes, int src, int dest)
-{
-PriorityQueue<Pair> pq = new PriorityQueue<>(); // easy to compare
-int n = graph.length;
+        public Pair(int node, int path)
+         {
+            this.node = node;
+            this.path = path;
+        }
 
-int dist[] = new int[n];
-boolean visited[] = new boolean[n];
-int parent[] = new int[n];
+        @Override
+        // this is for comparing in ascending order, for descending order we can reverse
 
-Arrays.fill(dist, Integer.MAX_VALUE);
-Arrays.fill(parent, -1);
+        public int compareTo(Pair p2) 
+        {
+            return Integer.compare(this.path, p2.path);
+        }
+    }
 
-if (nodes[src].
-blocked) // source is blocked
-{
-System.out.println("Source blocked");
-return new ArrayList<>();
-}
+    public static List shortestPath(ArrayList<edge>[] graph, node[] nodes, int src, int dest) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>(); // easy to compare
+        int n = graph.length;
 
-dist[src] = 0;
-pq.add(new Pair(src, 0));
+        int dist[] = new int[n];
+        boolean visited[] = new boolean[n];
+        int parent[] = new int[n];
 
-while (!
-pq.isEmpty())
-{
-Pair curr = pq.remove();
+        Arrays.fill(dist, Integer.MAX_VALUE);// initially all distances are infinity
+        Arrays.fill(parent, -1);//no parent initally
 
-if (!visited[curr.node])
-{
-visited[curr.node] = true;
-for (int i = 0; i < graph[curr.node].
-size(); i++)
-{
-edge e = graph[curr.node].
-get(i);
-int u = curr.node;
-int v = e.dest;
-if (e.blocked || nodes[v].
-blocked)
-continue;
+        if (nodes[src].blocked) // source is blocked
+        {
+            System.out.println("Source blocked");//u lose
+            return new ArrayList<>();
+        }
 
-if (dist[u] != Integer.MAX_VALUE && dist[u] + e.weight < dist[v])
-{
-dist[v] = dist[u] + e.weight;
-parent[v] = u;
-pq.add(new Pair(v, dist[v]));
-}
-}
-}
-}
+        dist[src] = 0;// distance to source is 0
+        pq.add(new Pair(src, 0));
 
-if (dist[dest] == Integer.MAX_VALUE) // no path found
-{
-return new ArrayList<>();
-}
+        while (!pq.isEmpty()) 
+            {
+            Pair curr = pq.remove();
 
-List path = new ArrayList<>();
-for (int i = dest; i != -1; i = parent[i])
-{
-path.add(i);
-}
-Collections.reverse(path);
-return path;
-}
+            if (!visited[curr.node]) 
+                {
+                visited[curr.node] = true;
+                for (int i = 0; i < graph[curr.node].size(); i++) 
+                    {
+                    edge e = graph[curr.node].get(i);
+                    int u = curr.node;
+                    int v = e.dest;
+                    if (e.blocked || nodes[v].blocked)
+                        continue;
+
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + e.weight < dist[v]) {
+                        dist[v] = dist[u] + e.weight;
+                        parent[v] = u;
+                        pq.add(new Pair(v, dist[v]));
+                    }
+                }
+            }
+        }
+
+        if (dist[dest] == Integer.MAX_VALUE) // no path found
+        {
+            return new ArrayList<>();
+        }
+
+        List path = new ArrayList<>();//
+        for (int i = dest; i != -1; i = parent[i]) {
+            path.add(i);
+        }
+        Collections.reverse(path);
+        return path;
+    }
 }
